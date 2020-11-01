5dc104f64638c98779d6f78b910a6d3921ca2d76
Zara <zara@gene.co.uk>
2020-11-01 14:08:05 +0100

    omg the login api is working

--- components/elements/FieldGroup.js
+++ components/elements/FieldGroup.js
@@ -1,10 +1,11 @@
-export default function FieldGroup( {id, label, inputType, value}) {
+export default function FieldGroup( {id, label, inputType, value, setMethod = null}) {
   return (
     <div className="form__group">
       <label htmlFor={id}>
         {label}
       </label>
-      <input type={inputType} name={id} className="form__input" />
+      <input type={inputType} name={id} className="form__input" value={value}
+          onChange={e => setMethod(e.target.value)}/>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,46 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Button from '../components/elements/Button'
 import FieldGroup from '../components/elements/FieldGroup'
+import { useState } from "react";
 
 export default function Home() {
+
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    var formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+
+    fetch('http://php-project.test/api/login', {
+      method: 'POST',
+      body:formData
+    }).then(response => response.json())
+    .then(
+      (result) => {
+
+        if (result.missingUsername || result.missingPassword) {
+          console.log('error');
+          return;
+        }
+
+        if (result.username && result.password) {
+          console.log('log me in');
+        }
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        console.log(error);
+      }
+    )
+  
+  }
+
   return (
     <div className="wrapper">
       <Head>
@@ -14,16 +52,24 @@ export default function Home() {
         <h1>Welcome to our site.</h1>
         <h2> Login </h2>
         <p>Please fill in your credentials to login.</p>
-        <form action="/login" method="post" className="form">
+        <form onSubmit={handleSubmit} className="form">
   
-          <FieldGroup id="username" label="Username" inputType="text"/>
-          <FieldGroup id="password" label="Password" inputType="password"/>
-
+          <FieldGroup 
+            id="username"
+            label="Username"
+            inputType="text"
+            value={username}
+            setMethod={setUserName}/>
+          <FieldGroup 
+            id="password"
+            label="Password"
+            inputType="password"
+            value={password}
+            setMethod={setPassword}/>
+          <div className="form__group actions">
+            <Button type="submit" text="Login"/>
+          </div>
         </form>
-        <div className="form__group actions">
-          <Button type="submit" text="Login"/>
-        </div>
-       
         <p>Don't have an account? <Link href="/register"><a>Sign up now</a></Link>.</p>
       </main>
     </div>
