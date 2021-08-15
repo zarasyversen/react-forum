79527f7a1a6814cd91a752415c3cd07eadc8a77a
Zara Syversen <zarasyversen@hotmail.com>
2021-08-15 22:15:23 +0200

    add global footer

---
+++ components/presentational/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer--main">
+      <p>Next.js Project by Zara Syversen</p>
+    </footer>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Footer from '../components/presentational/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
+  
 }
 
 export default MyApp
